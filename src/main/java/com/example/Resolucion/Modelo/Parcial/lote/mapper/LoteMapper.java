f5872f70476543e69c0ca9599da7675edfb2e91f
kevinraipane <raipanekn@gmail.com>
2026-05-18 16:38:39 -0300

    miselaneas - se agregan comentarios descriptivos

--- src/main/java/com/example/Resolucion/Modelo/Parcial/lote/mapper/LoteMapper.java
+++ src/main/java/com/example/Resolucion/Modelo/Parcial/lote/mapper/LoteMapper.java
@@ -11,3 +11,23 @@ public interface LoteMapper {
     @Mapping(source = "estante.id", target = "idEstante")
     LoteDTO toDTO(Lote lote);
 }
+
+/*
+ * lote.reactivo  → objeto Reactivo (con id, nombre, nivelPeligro...)
+ * lote.estante   → objeto Estante  (con id, codigo, capacidad...)
+ *
+ * pero LoteDTO (la response) tiene
+ * Integer idReactivo
+   Integer idEstante
+
+
+ * idReactivo  → Integer
+ * idEstante   → Integer
+ *
+ * entonces mapStruct le dice
+ *
+ * source = "reactivo.id" → navega al objeto reactivo y toma su campo id
+    target = "idReactivo" → ponelo en el campo idReactivo del DTO
+    *
+    * si no ponemos @Mapping mapStruct te deja esos campos como null, ya que no tienen el mismo nombre
+ */
