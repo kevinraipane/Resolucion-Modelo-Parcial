4f2193299c5933275372e99b6bc044ee6d92f8ca
kevinraipane <raipanekn@gmail.com>
2026-05-18 16:39:56 -0300

    update - comentario descriptivo

--- src/main/java/com/example/Resolucion/Modelo/Parcial/reactivo/dto/ReactivoCreateDTO.java
+++ src/main/java/com/example/Resolucion/Modelo/Parcial/reactivo/dto/ReactivoCreateDTO.java
@@ -8,4 +8,7 @@ public record ReactivoCreateDTO(
         @NotBlank String nombre,
         @NotNull @Positive Integer nivelPeligro,
         @NotNull Boolean esPrecursorQuimico
+        // Aqui no se setea el "activo" ya que MapStruct no puede saber este default de negocio,+
+        // Entonces lo hacemos en el service
+        // reactivo.setActivo(true);
 ) {}
